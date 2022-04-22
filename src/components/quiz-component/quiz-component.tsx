2db0abbc859bae93e789b6541e97f1db9d2517be
Henry Asante <henry@roninlabs.co>
2022-04-22 20:24:17 +0000

    Update quiz-component.tsx

--- src/components/quiz-component/quiz-component.tsx
+++ src/components/quiz-component/quiz-component.tsx
@@ -27,43 +27,67 @@ export class QuizComponent {
     let allow = this.data["blocks"].every(block => block.choice !== undefined);
     if (allow) {
       this.submit({ identifier: `${this.data["id"]}-${this.consumer}`, ...this.data }); // component+consumer
+    } else {
+      alert("Your interaction data/answers aren't valid")
     }
   }
 
+
+  // if submitted is true, render the submitted page instead
+  // find how many of the component answers match the choice
   Quizzed = () => {
     let index = 0;
     switch (this.data["comp"]) {
       case this.options[index].id: // single choice answering
         return (
           <div>
-            {this.data["blocks"].map((block, index) => (
-              <div class="mt-4" key={index}>
-                <h6 class="mb-3 text-bold"> {block.question} </h6>
+            {!this.submitted ?
+              <div>
+                {this.data["blocks"].map((block, index) => (
+                  <div class="mt-4" key={index}>
+                    <h6 class="mb-3 text-bold"> {block.question} </h6>
 
-                {block.answers.map((answer, index1) => (
-                  <div key={index1} class="form-check mt-2">
-                    <input
-                      class="form-check-input"
-                      type="radio"
-                      name={`block-${index}`}
-                      id="flexRadioDefault2"
-                      value={answer}
-                      checked={block.answers[block.answer].choice === index1}
-                      onInput={() => {
-                        let blocks = this.data["blocks"];
-                        blocks[index].choice = index1; // set the users answer to the block index
-                        this.data = { ...this.data, blocks };
-                      }}
-                    />
-                    <label class="form-check-label" htmlFor="flexRadioDefault2">{answer}</label>
+                    {block.answers.map((answer, index1) => (
+                      <div key={index1} class="form-check mt-2">
+                        <input
+                          class="form-check-input"
+                          type="radio"
+                          name={`block-${index}`}
+                          id="flexRadioDefault2"
+                          value={answer}
+                          checked={block.answers[block.answer].choice === index1}
+                          onInput={() => {
+                            let blocks = this.data["blocks"];
+                            blocks[index].choice = index1; // set the users answer to the block index
+                            this.data = { ...this.data, blocks };
+                          }}
+                        />
+                        <label class="form-check-label" htmlFor="flexRadioDefault2">{answer}</label>
+                      </div>
+                    ))}
                   </div>
                 ))}
+                <button type="button" class="btn btn-primary mt-4" onClick={this.SubmitQuiz}>Submit</button>
+                <p class="text-small mt-3"> {this.consumer ? "" : "Identification not present, please contact support"} </p>
               </div>
-            ))}
+              :
+              <div>
+                <h3 class="mt-4">You have completed this quiz successfully 🏆</h3>
+                <p class="text-small mb-4"> Your interaction data for this quiz has been saved successfully. </p>
 
-            <button type="button" class="btn btn-primary mt-4" onClick={this.SubmitQuiz}>Submit</button>
-            <p class="text-small mt-3"> {this.consumer ? "" : "Identification not present, please contact support"} </p>
+                <h4 class="text-small mt-5"> Your score: </h4>
+                <p class="text-small mt-1">
+                  {this.data["blocks"].filter(block => block.answer == block.choice).length} of {this.data["blocks"].length} Correct ({Math.round(this.data["blocks"].filter(block => block.answer == block.choice).length / this.data["blocks"].length * 100)}%)
+                </p>
 
+
+                <h4 class="text-small mt-5"> The Answers: </h4>
+                <p class="text-small mt-1">
+                  {/* Render questions,  */}
+                </p>
+
+              </div>
+            }
           </div>
         )
       case this.options[index = +1].id: // multi choice answering
@@ -85,8 +109,11 @@ export class QuizComponent {
     return (
       <Host>
         <slot>
-          <h3 class=""> {this.data["title"]} </h3>
-          <p class="mt-0"> {this.data["summary"]} </p>
+          {!this.submitted && <div>
+            <h3 class=""> {this.data["title"]} </h3>
+            <p class="mt-0"> {this.data["summary"]} </p>
+          </div>}
+
           {this.Quizzed()}
         </slot>
       </Host>
