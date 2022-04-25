60c07eca400bf097a108897fdfa1df563ffa8ddd
Henry Asante <henry@roninlabs.co>
2022-04-25 10:38:20 +0000

    New Updates with styling setup

--- src/components/learnir-exp-module/learnir-exp-module.tsx
+++ src/components/learnir-exp-module/learnir-exp-module.tsx
@@ -120,6 +120,10 @@ export class LearnirExpModule {
   render() {
     return (
       <Host>
+        <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh">
+        </link>
         <link rel="stylesheet" href="/build/learnir-exp-module/learnir-exp-module.css" />
         <link rel="stylesheet" href="/dist/learnir-exp-module/learnir-exp-module.css" />
         <link rel="stylesheet" href="/build/learnir-exp-module.css" />
@@ -135,6 +139,10 @@ export class LearnirExpModule {
             </div>
           }
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
       </Host>
     );
   }
